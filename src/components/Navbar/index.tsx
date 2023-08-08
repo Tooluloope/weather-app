f223c86f7b45004e1b0b7707b992826dc48ff9f2
Tolulope <adetulatolu@gmail.com>
2023-08-08 18:07:27 +0100

    fix: refactor and animate toggle

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,34 +9,42 @@ const Navbar: React.FC<{ inputRef: React.Ref<{ focus: () => void }> }> = ({
 
 	const renderThemeChanger = () => {
 		const currentTheme = theme === "system" ? systemTheme : theme;
+		const isDark = currentTheme === "dark";
 
-		if (currentTheme === "dark") {
-			return (
+		return (
+			<>
 				<SunIcon
-					className="w-10 h-10 text-yellow-500  "
+					className={`${
+						isDark
+							? "translate-x-[-50%] left-[50%]"
+							: "translate-x-[100%] left-[100%]"
+					} w-10 h-10 text-yellow-500  absolute transition duration-150 ease-in-out top-[50%] translate-y-[-50%]`}
 					role="button"
 					onClick={() => setTheme("light")}
 				/>
-			);
-		} else {
-			return (
+
 				<MoonIcon
-					className="w-10 h-10 text-blue-500 "
+					className={`${
+						!isDark
+							? "translate-x-[-50%] left-[50%]"
+							: "translate-x-[-100%] left-0"
+					} w-10 h-10 text-blue-500  absolute transition duration-150 ease-in-out  top-[50%] translate-y-[-50%] `}
 					role="button"
 					onClick={() => setTheme("dark")}
 				/>
-			);
-		}
+			</>
+		);
 	};
+
 	return (
 		<nav
 			data-testid="navbar"
-			className=" fixed top-0 left-0 w-screen  py-3  shadow z-50 bg-white dark:bg-slate-900"
+			className=" fixed top-0 left-0 w-screen  py-3  shadow z-50 "
 		>
 			<div className=" flex justify-between items-center container mx-auto px-5">
 				<Select ref={inputRef} />
 
-				<button className=" hover:bg-gray-100 dark:hover:bg-gray-800 text-white px-2 py-2 rounded-full ml-2 shrink-0">
+				<button className=" hover:bg-gray-100 dark:hover:bg-gray-800 text-white px-2 py-2 rounded-full ml-2 shrink-0 relative w-[56px] h-[56px] overflow-hidden">
 					{renderThemeChanger()}
 				</button>
 			</div>
